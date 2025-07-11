6e754c6078c1601aa13c073a74813a39a18f9f83
Serena Chen <serenaychen2005@gmail.com>
2025-07-11 13:36:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/*** Dark Mode ***
+
+  When To Modify:
+  - [ ] Project 5 (REQUIRED FEATURE) 
+  - [ ] Any time after
+***/
+
+let themeButton = document.getElementById("theme-button");
+
+const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+}
+
+themeButton.addEventListener("click", toggleDarkMode);
+
+
+/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
+/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
